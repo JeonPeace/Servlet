a6eb03ada4994f1e5a0ffef43302fb65a7c9455a
megait402 <megait402@gmail.com>
2024-07-23 21:12:40 +0900

    [전진우] - post 예제 추가 - post 문제 풀이 추가

---
+++ src/main/java/com/jeonpeace/servlet/servlet/test/Test09Controller.java
@@ -0,0 +1,35 @@
+package com.jeonpeace.servlet.servlet.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/test09")
+public class Test09Controller extends HttpServlet{
+	
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		String content = request.getParameter("content");
+		
+		out.println("<html><head><title>지원 완료</title></head>");
+		out.println("<body>");
+		out.println("<h1>" + name + "님 지원이 완료 되었습니다.</h1>");
+		out.println("<hr>");
+		out.println("지원 내용");
+		out.println("<br>");
+		out.println(content);
+		out.println("</body></html>");
+		
+	}
+
+}
